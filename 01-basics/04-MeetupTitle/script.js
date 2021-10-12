26b7b76addd5f6abf8b4c09c6ddc5460c37aa1ad
dkulinkin <dmitrii.kulinkin@gmail.com>
2021-10-12 12:43:52 +0300

    Решение задач: 00-intro/01-sum 01-basics/01-CounterButtonApp 01-basics/02-VueCalculator 01-basics/03-MarkedList 01-basics/04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,29 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const MeetupTitle = {
+  data() {
+    return {
+      meetupId: '1',
+      meetupTitle: '',
+    };
+  },
+  async created() {
+    await this.loadMeetupInfo(this.meetupId);
+  },
+  watch: {
+    async meetupId(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        await this.loadMeetupInfo(newValue);
+      }
+    },
+  },
+  methods: {
+    async loadMeetupInfo(id) {
+      const meetup = await fetchMeetupById(id);
+      this.meetupTitle = meetup.title;
+    },
+  },
+};
+
+createApp(MeetupTitle).mount('#app');
